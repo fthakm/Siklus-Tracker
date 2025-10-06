f1f96f92c4fc0ce8d108f85fead332701ce68c0a
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-06 14:02:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import DashboardPage from "./pages/Dashboard";
-import StudentsPage from "./pages/Students";
-import LatihanPage from "./pages/Latihan";
-import EvaluasiPage from "./pages/Evaluasi";
-import LeaderboardPage from "./pages/Leaderboard";
-import MainLayout from "./layout/MainLayout";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/layout/Sidebar";
+import Topbar from "./components/layout/Topbar";
+import ResponsiveDrawer from "./components/layout/ResponsiveDrawer";
+
+import DashboardPage from "./pages/DashboardPage";
+import SiswaPage from "./pages/SiswaPage";
+import LatihanPage from "./pages/LatihanPage";
+import EvaluasiPage from "./pages/EvaluasiPage";
+import LeaderboardPage from "./pages/LeaderboardPage";
+
+import "./App.css";
 
 export default function App() {
   return (
-    <Router>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/students" element={<StudentsPage />} />
-          <Route path="/latihan" element={<LatihanPage />} />
-          <Route path="/evaluasi" element={<EvaluasiPage />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-        </Routes>
-      </MainLayout>
-    </Router>
+    <div className="app-container flex min-h-screen bg-gray-50 text-gray-900 font-inter">
+      {/* Sidebar responsif (drawer di mobile) */}
+      <ResponsiveDrawer>
+        <Sidebar />
+      </ResponsiveDrawer>
+
+      {/* Konten utama */}
+      <main className="flex-1 flex flex-col overflow-hidden">
+        <Topbar />
+        <div className="flex-1 overflow-y-auto p-4 md:p-6">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/siswa" element={<SiswaPage />} />
+            <Route path="/latihan" element={<LatihanPage />} />
+            <Route path="/evaluasi" element={<EvaluasiPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+          </Routes>
+        </div>
+      </main>
+    </div>
   );
   }
